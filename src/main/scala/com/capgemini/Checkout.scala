4ccbb2da6388038adf2800b9de3c8c38fdedbf40
Andrew Todd <email.andy.todd@gmail.com>
2018-06-21 08:39:04 +0100

    Implement code to pass 'BOGOF apples' test

--- src/main/scala/com/capgemini/Checkout.scala
+++ src/main/scala/com/capgemini/Checkout.scala
@@ -1,25 +1,36 @@
 package com.capgemini
 
 import com.capgemini.Domain.Result
+import scala.math._
 
 class Checkout {
 
-  def apply(products: List[String]): Result[BigDecimal] =
-    products.foldLeft(Right(0): Result[BigDecimal]) {
-      case (totalResult, nextProduct) =>
+  def apply(products: List[String]): Result[BigDecimal] = {
+    val productsWithQuantities: Map[String, Int] = products.groupBy(x => x).mapValues(_.length)
+    productsWithQuantities.foldLeft(Right(0): Result[BigDecimal]) {
+      case (totalResult, (nextProduct, quantity)) =>
         for {
           total         <- totalResult
-          productPrice  <- getPrice(nextProduct)
+          productPrice  <- getPrice(nextProduct, quantity)
         } yield total + productPrice
     }
+  }
 
 
+  private val applePrice: BigDecimal = 0.60
+  private val orangePrice: BigDecimal = 0.25
 
-  private def getPrice(product: String): Result[BigDecimal] =
+  private def getPrice(product: String, quantity: Int): Result[BigDecimal] =
     product match {
-      case "Apple" => Right(0.60)
-      case "Orange" => Right(0.25)
+      case "Apple" => Right(buyOneGetOneFree(quantity, applePrice))
+      case "Orange" => Right(orangePrice * quantity)
       case unknown => Left(s"Product: '$unknown' is unknown")
     }
+
+  private def buyOneGetOneFree(quantity: Int, price: BigDecimal): BigDecimal = {
+    val bogofQuantity = ceil(quantity.toFloat / 2)
+    bogofQuantity * price
+  }
+
 
 }
